dfd0fe4a1e739bca35959fdf724feee030ef650f
Rafal Pietras <rafal.radx@gmail.com>
2024-01-27 18:39:57 +0100

    UDP client server established

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ APP_PORT = 3000
 
 class HttpHandler(BaseHTTPRequestHandler):
     udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # def __init__(self, udp_client: socket, *args, **kwargs):
-    #     self.udp_client = udp_client
-    #     super().__init__(*args, **kwargs)
+    udp_server_address = (UDP_IP, UDP_PORT)
 
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
@@ -39,8 +37,15 @@ class HttpHandler(BaseHTTPRequestHandler):
             key: value for key, value in [el.split("=") for el in data_parse.split("&")]
         }
         print(data_dict)
-        HttpHandler.udp_client.send("Testing message".encode())
-        print(HttpHandler.udp_client.recv(1024).decode())
+        message = "FLASH".encode()
+        HttpHandler.udp_client.sendto(message, HttpHandler.udp_server_address)
+        print(
+            f"CLIENT: Send data: '{message.decode()}' to UDP server: {HttpHandler.udp_server_address}"
+        )
+        response, address = HttpHandler.udp_client.recvfrom(1024)
+        print(
+            f"CLIENT: Response data: '{response.decode()}' from UDP server: {address}"
+        )
         self.send_response(302)
         self.send_header("Location", "/")
         self.end_headers()
@@ -64,30 +69,34 @@ class HttpHandler(BaseHTTPRequestHandler):
             self.wfile.write(file.read())
 
 
-def connect_udp_client(host, port) -> socket:
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((host, port))
-    udp_socket.connect()
-    return udp_socket
+# def connect_udp_client(host, port) -> socket:
+#     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     udp_socket.bind((host, port))
+#     udp_socket.connect()
+#     return udp_socket
 
 
 def run_udp_server(ip, port):
     udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_server.bind((ip, port))
     while True:
-        comm_socket, address = udp_server.accept()
-        message = comm_socket.recv(1024).decode()
-        print(message)
-        comm_socket.send("Message stored succesfully")
+        message, address = udp_server.recvfrom(1024)
+        print(
+            f"SERVER: Message received: '{message.decode()}' from UDP client {address}"
+        )
+        return_message = "THUNDER".encode()
+        udp_server.sendto(return_message, address)
+        print(
+            f"SERVER: Send responce: '{return_message.decode()}' to UDP client {address}"
+        )
 
 
 def run():
-    # udp_server_thread = threading.Thread(target=run_udp_server, args=(UDP_IP, UDP_PORT))
-    # udp_server_thread.start()
+    udp_server_thread = threading.Thread(target=run_udp_server, args=(UDP_IP, UDP_PORT))
+    udp_server_thread.start()
     # run_udp_server(UDP_IP, UDP_PORT)
-    udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_server.bind((UDP_IP, UDP_PORT))
-    comm_socket, address = udp_server.accept()
+    # udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # udp_server.bind((UDP_IP, UDP_PORT))
 
     # udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # http_handler = HttpHandler(udp_client=udp_client)
